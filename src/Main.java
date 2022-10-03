ddc0923bfbf1333a98e147dbf99020e9a5f68efa
User <333@fd.com>
2022-10-03 18:24:48 +0400

    Домашняя работа по уроку "Циклы"

--- src/Main.java
+++ src/Main.java
@@ -3,41 +3,47 @@ public class Main {
 
         //Задание 1
         for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+            System.out.print(" " +i );
         }
+        System.out.println("\n");
 
         //Задание 2
         for (int q = 10; q >= 1; q--) {
-            System.out.println(q);
+            System.out.print( " " + q );
         }
+        System.out.println("\n");
 
         //Задание 3
         for (int w = 2; w <= 17; w = w + 2) {
-            System.out.println(w);
+            System.out.print(" " + w );
         }
+        System.out.println("\n");
 
         //Задание 4
         for (int e = 10; e >= -10; e--) {
-            System.out.println(e);
+            System.out.print(" " + e );
         }
+        System.out.println("\n");
 
         //Домашнее задание 2
         //Задание1
         for (int year = 1904; year <= 2096; year = year + 4) {
             System.out.println(year + " год является високостным.");
         }
+        System.out.println("\n");
 
         //Задание 2
         for (int r = 7; r <= 98; r = r + 7) {
-            System.out.println(r);
+            System.out.print(" " + r);
         }
+        System.out.println("\n");
 
         //Задание 3
         int y = 1;
-        System.out.println(y);
         for (int t = 2; t <= 512; t = t * 2) {
-            System.out.println(t);
+            System.out.print(y + " " + t);
         }
+        System.out.println("\n");
 
         //Домашнее задание 3
         //Задание 1
